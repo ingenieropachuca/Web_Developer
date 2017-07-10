07bdf99faae5bd1915ef0b70ec7dfdfc08ad1c97
ingenieropachuca <ingenieropachuca@gmail.com>
2017-07-10 15:26:10 -0500

    aprendiendo JS

---
+++ alerta.js
@@ -0,0 +1,82 @@
+// var age = prompt("Whats your age: ");
+
+
+// if(age<21)
+// {
+// 	alert("You can enter but not drink");
+// }
+
+// else{
+// 	alert(age+" welcome sir");
+// }
+
+
+//AQUI EMPIEZA EL NUEVO EJERCICIO 
+/*
+var num=-10;
+
+while(num<=19)
+{
+console.log(num);
+num++;
+}
+
+console.log("NEW EXERCISE");
+
+var num1=10;
+while(num1<=40)
+{
+console.log(num1);
+num1+=2;
+}
+
+console.log("NEW EXERCISE 2");
+
+var num2=301;
+while(num2<=333)
+{
+console.log(num2);
+num2+=2;
+}
+
+console.log("NEW EXERCISE 3");
+
+var num3=5;
+while(num3<=55)
+{
+	if(num3%5===0 && num3%3===0)
+	{
+		console.log(num3);
+	}
+	num3++;
+}
+ */
+
+//AQUI EMPIEZA EL NUEVO EJERCICIO 2
+
+/* var palabra = prompt("Hola estas hay?");
+
+while(palabra !== "yes")
+{
+var palabra = prompt("Hola estas hay?");
+}
+*/
+
+//THE LAST EXERCISE 
+
+/*for (var num=-10;num<=19;num++)
+{
+console.log(num);
+}
+
+console.log("NUEVO EJERCICIO");
+	for(var num3=5; num3<=55; num3++)
+	{
+	if(num3%5===0 && num3%3===0)
+	{
+		console.log(num3);
+	}
+}*/
+var num = prompt("Hola escoge un numero del 1 al 10:  ");
+var numero= (((((num%10)*2)+3)/3)+1);
+alert("El numero es: "+nuevo);
