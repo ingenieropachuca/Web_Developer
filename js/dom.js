cf34a79c2e2e4c60941cb478bb05e846f1706e0c
ingenieropachuca <ingenieropachuca@gmail.com>
2017-07-14 22:16:54 -0500

    DOM Manipulation JS

---
+++ js/dom.js
@@ -0,0 +1,14 @@
+/*
+var tag = document.getElementById("high");
+var tags = document.getElementsByTagName("ul");
+var tags2 = document.getElementsByClassName("bolded");
+
+var h1= document.querySelector("hi");
+var li= document.querySelector("#high");
+var li= document.querySelector(".bolded");
+
+//TOMA TODOS LOS li
+var lis = document.querySelectorAll("li");
+//TOMA SOLO UN li el primero 
+var lis2 = document.querySelector("li");
+*/
